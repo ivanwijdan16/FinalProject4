202964490798b778ceccddd5d36e80693067495d
ivanwijdan16 <ivanok152@gmail.com>
2023-11-29 16:37:15 +0700

    add: back-to-top

--- script.js
+++ script.js
@@ -21,3 +21,24 @@ if (window.matchMedia("(min-width: 768px)").matches) {
 } else {
   $(multipleCardCarousel).addClass("slide");
 }
+
+document.addEventListener("DOMContentLoaded", function () {
+  var mybutton = document.getElementById("back-to-top");
+
+  window.onscroll = function () {
+    scrollFunction();
+  };
+
+  function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+      mybutton.style.display = "block";
+    } else {
+      mybutton.style.display = "none";
+    }
+  }
+
+  mybutton.addEventListener("click", function () {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+  });
+});
